5b18a802f04f751f8ed65428eb24299cea10c92f
Witold Wolski <wewolski@gmail.com>
2019-11-14 10:38:56 +0100

    cosmetics

---
+++ vignettes/renderAllVignettest.R
@@ -0,0 +1,5 @@
+rmarkdown::render("ExpDesign.Rmd")
+rmarkdown::render("Hypothesis_Testing_p3003.Rmd")
+rmarkdown::render("IntroductionToLinearModels.Rmd")
+rmarkdown::render("LFQ_Service_Examples.Rmd")
+rmarkdown::render("Multiplicity_p3003.Rmd")
