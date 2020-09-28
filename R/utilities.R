27eaa398238491e7a18af8a8a73cbef17f1aa869
Witold Wolski <wewolski@gmail.com>
2020-09-28 18:33:08 +0200

    moved sample_stasts to R/

---
+++ R/utilities.R
@@ -0,0 +1,33 @@
+
+#' sample_stats
+#' @export
+sample_stats <- function(N_obs = 12, dist1, dist2, samples = 100){
+
+  xx <- combn(N_obs, N_obs/2)
+  res_p <- matrix(nrow=samples, ncol=4)
+  colnames(res_p) <- c("our","coin","t.test","asymp.test")
+  means <- NULL
+  h_0_norm <- NULL
+  t_obs <- NULL
+  for (i in 1:samples) {
+    sample <- data.frame(intensity = c(dist1(N_obs/2), dist2(N_obs/2)),
+                         treatment = c(rep("A", N_obs/2), rep("B", N_obs/2)), stringsAsFactors = TRUE)
+
+    teststat <- function(i, comb, data){res <- mean(data[comb[,i],1]) - mean(data[-comb[,i],1]); return(res)}
+    h_0_norm <- sapply(1:ncol(xx), teststat, xx, sample)
+
+    means <- aggregate(intensity ~ treatment, sample, mean)
+    t_obs <- means[2,2] - means[1,2]
+
+    our <- sum(abs(t_obs) <= abs(h_0_norm)) / length(h_0_norm)
+    resindtest <- coin::independence_test(intensity ~ treatment, data = sample ,distribution = coin::exact())
+    coin <- coin::pvalue(resindtest)
+    t.test <- t.test(intensity~ treatment, data = sample)$p.value
+    asymp.test <- asympTest::asymp.test(intensity~ treatment, data = sample)$p.value
+
+    res_p[i,] <- c(our, coin, t.test, asymp.test)
+  }
+  return(list(res_p = res_p, sample=sample , means=means, h_0_norm = h_0_norm, t_obs =t_obs))
+}
+
+
