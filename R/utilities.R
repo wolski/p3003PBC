83310542855f3d7a4975c95ac475508949ed6bfd
Witold Wolski <wewolski@gmail.com>
2021-02-20 18:03:47 +0100

    updated utility functions

--- R/utilities.R
+++ R/utilities.R
@@ -1,25 +1,30 @@
 
 #' sample_stats
 #' @export
-sample_stats <- function(N_obs = 12, dist1, dist2, samples = 100){
+sample_stats <- function(N_obs = 12, dist1, dist2, samples = 100, trafo = identity){
 
-  xx <- combn(N_obs, N_obs/2)
+
   res_p <- matrix(nrow=samples, ncol=4)
   colnames(res_p) <- c("our","coin","t.test","asymp.test")
   means <- NULL
   h_0_norm <- NULL
   t_obs <- NULL
   for (i in 1:samples) {
-    sample <- data.frame(intensity = c(dist1(N_obs/2), dist2(N_obs/2)),
+    sample <- data.frame(intensity = c(trafo(dist1(N_obs/2)), trafo(dist2(N_obs/2))),
                          treatment = c(rep("A", N_obs/2), rep("B", N_obs/2)), stringsAsFactors = TRUE)
 
-    teststat <- function(i, comb, data){res <- mean(data[comb[,i],1]) - mean(data[-comb[,i],1]); return(res)}
-    h_0_norm <- sapply(1:ncol(xx), teststat, xx, sample)
+    mrand <- function(N_obs, sample ){
+      xx <- combn(N_obs, N_obs/2)
+      teststat <- function(i, comb, data){res <- mean(data[comb[,i],1]) - mean(data[-comb[,i],1]); return(res)}
+      h_0_norm <- sapply(1:ncol(xx), teststat, xx, sample)
+      means <- aggregate(intensity ~ treatment, sample, mean)
+      t_obs <- means[2,2] - means[1,2]
+      our <- sum(abs(t_obs) <= abs(h_0_norm)) / length(h_0_norm)
+      return(our)
+    }
 
-    means <- aggregate(intensity ~ treatment, sample, mean)
-    t_obs <- means[2,2] - means[1,2]
+    our <- mrand(N_obs, sample)
 
-    our <- sum(abs(t_obs) <= abs(h_0_norm)) / length(h_0_norm)
     resindtest <- coin::independence_test(intensity ~ treatment, data = sample ,distribution = coin::exact())
     coin <- coin::pvalue(resindtest)
     t.test <- t.test(intensity~ treatment, data = sample)$p.value
