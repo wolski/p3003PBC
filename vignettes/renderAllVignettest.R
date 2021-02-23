40a7d3e5aa0dd2ad2399690cce29b0df89390e88
wolski <wewolski@gmail.com>
2021-02-23 15:36:31 +0100

    vignettes ignore

--- vignettes/renderAllVignettest.R
+++ vignettes/renderAllVignettest.R
@@ -1,6 +1,7 @@
 rmarkdown::render("ExpDesign.Rmd")
+rmarkdown::render("Multiplicity_p3003.Rmd")
 rmarkdown::render("Hypothesis_Testing_p3003.Rmd")
+
 rmarkdown::render("IntroductionToLinearModels.Rmd")
 rmarkdown::render("LFQ_Service_Examples.Rmd")
-rmarkdown::render("Multiplicity_p3003.Rmd")
 rmarkdown::render("OtherSoftware.Rmd")
