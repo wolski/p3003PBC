85ac4d2284a873e16c376539e419429638da2440
Witold Wolski <wewolski@gmail.com>
2025-11-26 13:36:42 +0100

    Course Quant 20251125

--- vignettes/renderAllVignettest.R
+++ vignettes/renderAllVignettest.R
@@ -1,14 +1,43 @@
-rmarkdown::render("ExpDesign.Rmd")
-rmarkdown::render("ExpDesignProteomics.Rmd")
-rmarkdown::render("Hypothesis_Testing_p3003.Rmd")
-rmarkdown::render("IntroductionToLinearModels.Rmd")
-rmarkdown::render("Multiplicity_p3003.Rmd")
-rmarkdown::render("SampleSizeEstimation.Rmd")
-rmarkdown::render("GSEA_with_msig.Rmd")
-rmarkdown::render("OtherSoftware.Rmd")
-rmarkdown::render("BriefIntroductionTooR.Rmd")
-rmarkdown::render("ImpactOfFilteringOnProteinFDR.Rmd")
-rmarkdown::render("BioCED2019.Rmd")
+# List of all vignettes
+vignettes <- c(
+  "ExpDesignShort.Rmd",
+  "FASTA_ProteinInference.Rmd",
+  "GSEA_Slides.Rmd",
+  "Multiplicity_Simple.Rmd",
+  "ExpDesign.Rmd"
+#  "ExpDesignProteomics.Rmd",
+#  "Hypothesis_Testing_p3003.Rmd",
+#  "IntroductionToLinearModels.Rmd",
+#  "Multiplicity_p3003.Rmd",
+#  "SampleSizeEstimation.Rmd",
+#  "GSEA_with_msig.Rmd",
+#  "OtherSoftware.Rmd",
+#  "BriefIntroductionTooR.Rmd",
+#  "ImpactOfFilteringOnProteinFDR.Rmd",
+#  "BioCED2019.Rmd"
+)
+
+# Render all vignettes to HTML
+for (v in vignettes) {
+  rmarkdown::render(v)
+}
 
 # Render index page last (after all vignettes are generated)
 rmarkdown::render("index_vignettes.Rmd")
+
+# Print all HTML files to PDF using pagedown
+# Use full file path to avoid relative path issues with images
+html_files <- sub("\\.Rmd$", ".html", vignettes)
+
+# Create pdf subfolder if it doesn't exist
+if (!dir.exists("pdf")) {
+  dir.create("pdf")
+}
+
+for (h in html_files) {
+  if (file.exists(h)) {
+    full_path <- normalizePath(h)
+    pdf_name <- file.path("pdf", sub("\\.html$", ".pdf", h))
+    pagedown::chrome_print(full_path, output = pdf_name, timeout = 20)
+  }
+}
